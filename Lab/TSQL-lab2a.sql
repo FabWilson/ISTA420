058f1d45bb0b349b1eb139afb86eefb2a19a2ed8
Fabiola Wilson <fabiolawilson14@gmail.com>
2018-08-14 13:02:51 -0400

    initial commit

---
+++ Lab/TSQL-lab2a.sql
@@ -0,0 +1,28 @@
+.echo on
+.headers on
+
+--Name: Fabiola Wilson
+--File: TSQL-lab2a.sql
+--Date; August 14, 2018
+
+--What are the regions?
+
+
+--What are the cities?
+
+
+--What are the cities in the southern region?
+ 
+--How do you run this query with a fully qualified column name? 
+
+--How do you run this query with a table alias?
+
+--What is the contact name, telephone number, and city for each customer?
+
+--What are the products currently out of stock?
+
+--What are the ten products currently in stock with least amount on hand?
+
+--What are the five most expensive products in stock?
+
+--How many products does Northwind have? How many customers? How manysupplies?  
